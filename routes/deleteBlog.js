fa357e760fa726de689e9d882f2968f3b6c73067
tusharb05 <tusharbaweja2005@gmail.com>
2021-10-10 23:17:46 +0530

    minor fix

--- routes/deleteBlog.js
+++ routes/deleteBlog.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const Blog = require("../models/Blog");
+const Comment = require("../models/Comment");
 
 router.post("/", async (req, res) => {
   const { blogID } = req.body;
   const deletedBlog = await Blog.deleteOne({ _id: blogID });
+
+  const deletedComments = await Comment.deleteMany({ blogID: blogID });
+
   if (deletedBlog) return res.json({ status: "deleted" });
   res.json({ status: "failure" });
 });
