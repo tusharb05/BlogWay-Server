26849050db50f27054e3d43da6be2e810f5f539b
tusharb05 <tusharbaweja2005@gmail.com>
2021-10-10 16:19:29 +0530

    delete comment update

--- routes/deleteComment.js
+++ routes/deleteComment.js
@@ -1,11 +1,15 @@
 const Comment = require("../models/Comment");
 const router = require("express").Router();
+const Blog = require("../models/Blog");
 
 router.post("/", async (req, res) => {
-  const { commentID } = req.body;
+  const { commentID, blogID } = req.body;
   const deleted = await Comment.findByIdAndDelete(commentID);
+  const updated = await Blog.findByIdAndUpdate(blogID, {
+    $inc: { commentCount: -1 },
+  });
   if (deleted) {
-    const comments = await Comment.find({});
+    const comments = await Comment.find({ _id: blogID });
     res.json({ comments });
   } else {
     res.json({ status: "failure" });
